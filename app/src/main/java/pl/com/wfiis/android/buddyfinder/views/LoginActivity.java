4300c1a024a150ccc67f55b7640441351a11fa04
unknown <ul0249604@edu.uni.lodz.eu>
2022-08-15 22:36:33 +0200

    added registration

---
+++ app/src/main/java/pl/com/wfiis/android/buddyfinder/views/LoginActivity.java
@@ -0,0 +1,16 @@
+package pl.com.wfiis.android.buddyfinder.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.com.wfiis.android.buddyfinder.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/pl/com/wfiis/android/buddyfinder/views/RegisterActivity.java
@@ -0,0 +1,112 @@
+package pl.com.wfiis.android.buddyfinder.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import pl.com.wfiis.android.buddyfinder.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText emailField,passwordField,usernameField;
+    Button registerBtn;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailField = findViewById(R.id.registerEmail);
+        passwordField = findViewById(R.id.registerPassword);
+        usernameField = findViewById(R.id.registerUsername);
+        registerBtn = findViewById(R.id.registerButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = emailField.getText().toString().trim();
+                final String username = usernameField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    emailField.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    passwordField.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    passwordField.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            firebaseUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegisterActivity.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d( "TAG","onFailure: Email not sent " + e.getMessage());
+                                }
+                            });
+
+                            //TO ADD MAYBE SOME ADDITIONAL DATA TO USER DB
+                            Toast.makeText(RegisterActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                            userID = firebaseAuth.getCurrentUser().getUid();
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+
+                        else {
+                            Toast.makeText(RegisterActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+    }
+}
